18b8756611eae444e486f9b6c0cd25c1c9e43fc3
haijitianlin <102601458+haijitianlin@users.noreply.github.com>
2025-05-10 12:21:14 +0800

    Update vless源码.js

--- Vless_workers_pages/vless源码.js
+++ Vless_workers_pages/vless源码.js
@@ -4,9 +4,9 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "f690a2ab-c345-4832-b685-c7e55ed35b03";
 
-const proxyIPs = ["xjp.ygkkk.dpdns.org:60360"];
+const proxyIPs = ["tw.azaleas.sbs"];
 const cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
